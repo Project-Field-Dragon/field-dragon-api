45544e16aa9c4562d4d11022f82e68bc84e6b4a3
Hsiangyu Lan <Lan.227@buckeyemail.osu.edu>
2025-11-06 18:56:43 -0500

    feat: add all catalog api endpoints (part 1)

---
+++ Project.Field.Dragon.Domain/Catalog/Rating.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Project.Field.Dragon.Domain.Catalog
+{
+    public class Rating
+    {
+        // 建構函式 (Constructor)
+        public Rating(int stars, string userName, string review)
+        {
+            if (stars < 1 || stars > 5)
+            {
+                throw new ArgumentException("Star rating must be an integer of: 1, 2, 3, 4, 5.");
+            }
+
+            if (string.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException("UserName cannot be null.");
+            }
+
+            this.Stars = stars;
+            this.UserName = userName;
+            this.Review = review;
+        }
+
+        // 屬性 (Properties)
+        public int Stars { get; set; }
+        public string UserName { get; set; }
+        public string Review { get; set; }
+    }
+}
